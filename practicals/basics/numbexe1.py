9b76afd9a43e34fa549bf3aa86eb770f6805b6a3
Sangay Rinchen <sangaylama831@gamil.com>
2024-08-30 16:04:03 +0600

    lama

---
+++ practicals/basics/numbexe1.py
@@ -0,0 +1,20 @@
+age = 20
+print("My age is:",age)
+
+height = "1.71cm"
+print("My height is:",height)
+
+age_in_days = age*365
+print("My age in days is:",age_in_days)
+
+result = age/7
+print("My age divided by 7 is:",result)
+
+
+
+
+
+
+
+
+
---
+++ practicals/basics/string.py
@@ -0,0 +1,11 @@
+name = "Sangay Rinchen Tamang"
+print("My name is:",name)
+
+greet = "Hello, " + name + "!"
+print(greet)
+
+greet_f = f"Hello, {name}!"
+print(greet_f)
+
+name_length = len(name)
+print("The length of my name is:",name_length)
